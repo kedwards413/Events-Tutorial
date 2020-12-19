51e4c45dbcd15d63c6e72417456ea19322c54b31
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-19 01:03:21 +0000

    learning

--- .breathecode/resets/07.1-change-turn-on-click/index.js
+++ .breathecode/resets/07.1-change-turn-on-click/index.js
@@ -2,14 +2,14 @@ var currentUser = "Mario";
 
 window.onload = function loadfn() {
 	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
-};
+}
 
 window.turnChanger = function turnChanger() {
 	if (currentUser == "Mario") {
 		currentUser = "Juan";
-	} else if (currentUser == "Juan") {
-		currentUser = "Josh";
-	} else currentUser = "Mario";
+	} else {
+		currentUser = "Mario";
+	}
 
 	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
-};
+}
